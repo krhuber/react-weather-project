9b96b3defcd258e64bdc163052c3454b0cfbd7fb
krhuber <kr.huber@gmail.com>
2022-10-04 14:27:40 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://app.netlify.com/sites/classy-swan-70b63b/overview"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kathryn Huber
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/krhuber/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://app.netlify.com/sites/classy-swan-70b63b/overview"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kathryn Huber
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/krhuber/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
