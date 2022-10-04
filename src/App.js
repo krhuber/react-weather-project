70eeb361bd9fe6ec0c791daf96f6f1a0f5ce5934
krhuber <kr.huber@gmail.com>
2022-10-04 13:58:56 -0400

    Add GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://app.netlify.com/sites/classy-swan-70b63b/overview"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kathryn Huber
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/krhuber/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
