ae1e97189cd366b9a6eb58cc90df53040ef9235d
krhuber <kr.huber@gmail.com>
2022-10-20 14:15:58 -0400

    Used correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       feelsLike: response.data.main.feels_like,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
